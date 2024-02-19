cfeefd75f4b72b83e572fedecf2d59d647163fd3
Kuldeepdhakar6268 <dhakarkuldeep6268@gmail.com>
2024-02-20 01:34:52 +0530

    padding

---
+++ objects.js
@@ -0,0 +1,11 @@
+let myobj={
+    "my_name":"kuldeep dhakar",
+    "age":20,
+    address:"Gwalior",
+    bankdetail:{
+       AccountNumber:2323,
+       fathername:"Basdev"
+    }
+}
+let {my_name,age,address}=myobj;
+console.log(my_name);
